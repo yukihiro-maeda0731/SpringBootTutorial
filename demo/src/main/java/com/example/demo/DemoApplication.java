bb8af92674d6dfdfa4c4f517d142c556df406bf4
前田 <sloth19920731@gmail.com>
2022-01-12 22:10:45 +0900

    ２３：２０まで:モデルを返すAPIに変更

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,15 @@
 package com.example.demo;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.student.Student;
 
 @SpringBootApplication
 @RestController
@@ -16,8 +20,8 @@ public class DemoApplication {
 	}
 
 	@GetMapping
-	public List<String> hello() {
-		return List.of("Hello", "World");
+	public List<Student> hello() {
+		return List.of(new Student(1L,"Mariam","Mariam.gmail.com",LocalDate.of(2000, Month.JANUARY,5),21));
 	}
 
 }
