fe35df86eabfe2a98785eadb56a8fe001490cd1f
前田 <sloth19920731@gmail.com>
2022-01-14 23:43:22 +0900

    29:10まで：サービス作成

--- demo/src/main/java/com/example/demo/student/StudentController.java
+++ demo/src/main/java/com/example/demo/student/StudentController.java
@@ -1,7 +1,5 @@
 package com.example.demo.student;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,9 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path="api/v1/student")
 public class StudentController {
 
+	public StudentController(StudentService studentService) {
+		this.studentService = studentService;
+	}
+
+	private final StudentService studentService;
+
 	@GetMapping
 	public List<Student> getStudents() {
-		return List.of(new Student(1L,"Mariam","Mariam.gmail.com",LocalDate.of(2000, Month.JANUARY,5),21));
+		return studentService.getStudents();
 	}
 
 }
