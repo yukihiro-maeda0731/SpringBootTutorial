dd8e08608d57c0373983f4e9a75f24d9734de3b9
前田 <sloth19920731@gmail.com>
2022-01-15 23:42:09 +0900

    32:48まで：アノテーションでインスタンス生成省略など

--- demo/src/main/java/com/example/demo/student/StudentController.java
+++ demo/src/main/java/com/example/demo/student/StudentController.java
@@ -2,6 +2,7 @@ package com.example.demo.student;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path="api/v1/student")
 public class StudentController {
 
+	@Autowired
 	public StudentController(StudentService studentService) {
 		this.studentService = studentService;
 	}
