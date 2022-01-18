88b15764fa12264adcd708e863baab09d8f882eb
前田 <sloth19920731@gmail.com>
2022-01-18 23:33:01 +0900

    58:52:アプリ起動時に、決め打ちでにゅうりょくしたデータをポスグレに作成

--- demo/src/main/java/com/example/demo/student/StudentService.java
+++ demo/src/main/java/com/example/demo/student/StudentService.java
@@ -1,15 +1,21 @@
 package com.example.demo.student;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
 
+	private final StudentRepository studentRepository;
+
+	@Autowired
+	public StudentService(StudentRepository studentRepository) {
+		this.studentRepository = studentRepository;
+	}
+
 	public List<Student> getStudents() {
-		return List.of(new Student(1L,"Mariam","Mariam.gmail.com",LocalDate.of(2000, Month.JANUARY,5),21));
+		return studentRepository.findAll();
 	}
 }
